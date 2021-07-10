b321b5ec1081e663b504e5137ed8f66fa1be02a9
panos <panververis@gmail.com>
2021-07-10 19:08:33 +0100

    l;kh

--- hello-world.ts
+++ hello-world.ts
@@ -2,14 +2,14 @@
 let helloWorldMessage: string = 'Hello, World!';
 try {
     let a = 5+5;
+    let intArray = [1, 3, 5];
+
+    intArray.forEach(x => console.log(x));
 }catch(error) {
     console.error(error);
 }finally{
     console.log(helloWorldMessage);
 }
-var something = 5;
-
-
 
 export function addTwoNumbers(firstNumber: number, secondNumber : number) : number{
     return firstNumber + secondNumber;
