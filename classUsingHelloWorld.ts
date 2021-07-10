b959a23f081b3367a73486ef4a97443a8f493f1c
panos <panververis@gmail.com>
2021-07-10 16:48:29 +0100

    jhtnk

---
+++ classUsingHelloWorld.ts
@@ -0,0 +1,4 @@
+import { addTwoNumbers } from "./hello-world";
+
+let something = addTwoNumbers(5, 10);
+console.log(something);
--- hello-world.ts
+++ hello-world.ts
@@ -1,2 +1,16 @@
+"use strict";
 let helloWorldMessage: string = 'Hello, World!';
-console.log(helloWorldMessage);
+try {
+    let a = 5+5;
+}catch(error) {
+    console.error(error);
+}finally{
+    console.log(helloWorldMessage);
+}
+var something = 5;
+
+
+
+export function addTwoNumbers(firstNumber: number, secondNumber : number) : number{
+    return firstNumber + secondNumber;
+}
